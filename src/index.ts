6d76949fb6938479ddad12e4d160ccb7ead5665f
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-02-06 15:17:55 +0100

    Refactor to use AbortController

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,25 @@ export default function makeCancellablePromise<T>(promise: Promise<T>): {
   promise: Promise<T>;
   cancel(): void;
 } {
-  let isCancelled = false;
+  const controller = new AbortController();
+  const signal = controller.signal;
 
   const wrappedPromise: Promise<T> = new Promise((resolve, reject) => {
-    promise
-      .then((value) => !isCancelled && resolve(value))
-      .catch((error) => !isCancelled && reject(error));
+    if (signal.aborted) {
+      reject(signal.reason);
+    }
+
+    promise.then(resolve).catch(reject);
+
+    signal.addEventListener('abort', () => {
+      reject(signal.reason);
+    });
   });
 
   return {
     promise: wrappedPromise,
     cancel() {
-      isCancelled = true;
+      controller.abort();
     },
   };
 }
